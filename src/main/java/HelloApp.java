d63028052a55ac596acb58513fef8bd0164822b6
divya-singh07 <divyasjadoun@gmail.com>
2026-04-06 14:44:40 +0530

    Coded UC5: Enhanced for loop greeting

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,19 +1,20 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String message;
-
-        // Check if arguments are provided
-        if (args.length > 0) {
-            // Join all names with comma and space
-            String names = String.join(", ", args);
-            message = "Hello, " + names + "!";
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            // Default message
-            message = "Hello, World!";
-        }
 
-        // Print final message
-        System.out.println(message);
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
